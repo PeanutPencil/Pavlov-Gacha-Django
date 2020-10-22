8dc2f591dff6087b674f7c1dbca54c18856a31c9
Mark Wingerd <mawingerd@gmail.com>
2020-10-21 21:34:32 -0700

    Add serializer and views

--- pgserv/cards/views.py
+++ pgserv/cards/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from cards.models import Card
+from cards.serializers import CardSerializer
+
+
+class CardList(APIView):
+    """ List all cards or create a new card """
+    def get(self, request):
+        cards = Card.objects.all()
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CardDetail(APIView):
+    """
+    Retrieve, update or delete a card instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Card.objects.get(pk=pk)
+        except Card.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        card = self.get_object(pk)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
