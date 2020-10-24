82a55eb462f9034836bc5c396c013247390a7951
Mark Wingerd <mawingerd@gmail.com>
2020-10-24 15:53:46 -0700

    Improve scraper

--- pgserv/libs/scrape.py
+++ pgserv/libs/scrape.py
@@ -1,10 +1,9 @@
 import requests
+import shutil
 
 from bs4 import BeautifulSoup
 
-# from cards.models import Card
-
-
+BASE_VRML_URL = 'https://vrmasterleague.com'
 
 WEAPONS = {
     'm4':           1,
@@ -41,31 +40,51 @@ WEAPONS = {
 
 
 def get_line_data(line):
-    """ Returns (name, picture, team) for a given line """
+    """ Returns (id, name, picture, team) for a given line """
     cells = line.find_all('td')
+    id_ = line.find("td", class_="player_cell").find("a")['href'].split('/')[-1]
     image = line.find("td", class_="player_cell").img['src']
     name = line.find("td", class_="player_cell").find('span').text
     team = line.find("td", class_="team_cell").find('span').text
-    return (name, image, team)
+
+
+    return (id_, name, image, team)
 
 
 if __name__ == '__main__':
     for i in range(0, 500, 100):
-        resp = requests.get(f'https://vrmasterleague.com/Pavlov/Players/List/?posMin={i}')
+        # Get vrml page with posMin value
+        resp = requests.get(f'{BASE_VRML_URL}/Pavlov/Players/List/?posMin={i}')
+        # Ensure there was no error
         if resp.status_code != 200:
             raise('you fucked up bitch')
 
+        # Get a BeautifulSoup object to scrape the data
         soup = BeautifulSoup(resp.text, 'html.parser')
 
+        # Find all lines in the table
         for line in soup.find_all('tr')[1:]:
-            name, image, team = get_line_data(line)
+            try:
+                # Get the id, name, image, and team from a given line
+                id_, name, image, team = get_line_data(line)
+            except AttributeError as e:
+                print(e)
+                print(line)
+                continue
 
+            # Download and save the image
+            response = requests.get(f'{BASE_VRML_URL}{image}', stream=True)
+            with open(image, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+
+            # Create cards in the db which are unique to player and weapon
             for weapon, rarity in WEAPONS.items():
-                # Card(name=name, weapon=weapon, rarity=rarity).save()
                 r = requests.post('http://127.0.0.1:8000/cards/', data={
                     'name': name,
                     'weapon': weapon,
                     'rarity': rarity,
+                    'image': image,
+                    'vrml_id': id_,
                 })
                 print(r.status_code, r.json()['name'])
 
