bcb3972c738e0bed85805795895f9c7621cca469
Peanut <carlson.jonathanluke@gmail.com>
2020-10-23 22:16:43 -0500

    created users

--- pgserv/users/views.py
+++ pgserv/users/views.py
@@ -1,46 +1,44 @@
-from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from cards.models import Card
-from cards.serializers import CardSerializer
+from users.models import User   
+from users.serializers import UserSerializer
 from django.http import Http404
 
-
-class CardList(APIView):
-    """ List all cards or create a new card """
+class UserList(APIView):
     def get(self, request):
-        cards = Card.objects.all()
-        serializer = CardSerializer(cards, many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-
+    
     def post(self, request):
-        serializer = CardSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CardDetail(APIView):
+class UserDetail(APIView):
     """
-    Retrieve, update or delete a card instance.
+     Retrieve, update or delete a user instance.
     """
     def get_object(self, pk):
         try:
-            return Card.objects.get(pk=pk)
-        except Card.DoesNotExist:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        card = self.get_object(pk)
-        serializer = CardSerializer(card)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        card = self.get_object(pk)
-        serializer = CardSerializer(card, data=request.data)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -50,3 +48,4 @@ class CardDetail(APIView):
         card = self.get_object(pk)
         card.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
