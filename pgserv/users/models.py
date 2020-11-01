8cea2902242a49860df9a2dc58f9c5602f412970
Mark Wingerd <mawingerd@gmail.com>
2020-10-31 21:07:36 -0700

    Various updates

--- pgserv/users/models.py
+++ pgserv/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=64)
     rolls = models.IntegerField()
     claims = models.IntegerField()
